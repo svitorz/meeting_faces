4432e7059d0ce09dbe40ae9d12eec91b76f1d5a8
svitorz <vitor.fabio.castro@gmail.com>
2023-10-04 07:28:54 -0300

    04.10
    Mudanças na documentação

--- Web/MeetingFaces/login.php
+++ Web/MeetingFaces/login.php
@@ -23,18 +23,18 @@ try{
         $_SESSION['ADM'] = FALSE;
         redireciona();
     }else{
-    $sql = "SELECT * FROM administrador WHERE email = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$email]);
-    $row = $stmt->fetch();
-        if(password_verify($senha,$row['senha'])){
-            $_SESSION['id_administrador'] = $row['id_administrador'];
-            $_SESSION['primeiro_nome'] = $row['primeiro_nome'];
-            $_SESSION['segundo_nome'] = $row['segundo_nome'];
-            $_SESSION['email'] = $row['email'];
-            $_SESSION['ADM'] = TRUE;
-            $_SESSION['USUARIO'] = FALSE;
-            redireciona();
+        $sql = "SELECT * FROM administrador WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$email]);
+        $row = $stmt->fetch();
+            if(password_verify($senha,$row['senha'])){
+                $_SESSION['id_administrador'] = $row['id_administrador'];
+                $_SESSION['primeiro_nome'] = $row['primeiro_nome'];
+                $_SESSION['segundo_nome'] = $row['segundo_nome'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['ADM'] = TRUE;
+                $_SESSION['USUARIO'] = FALSE;
+                redireciona();
         }
     }
 } catch(Exception $e){
