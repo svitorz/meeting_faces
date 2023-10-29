ec6902f6eed378b046b536ebf16ef87266d79916
Vitor Castro <vitor.fabio.castro@gmail.com>
2023-10-29 17:34:05 -0300

    Correção de erros

--- Web/MeetingFaces/formulario-feedback.php
+++ Web/MeetingFaces/formulario-feedback.php
@@ -3,7 +3,6 @@ session_start();
 
 require 'logica.php';
 
-
 //Permite que apenas usuários autenticados acessem o formulário de feedback
 if(!autenticado()){
     $_SESSION['restrito'] = true;
@@ -15,6 +14,18 @@ $id_morador = filter_input(INPUT_GET, 'id_morador', FILTER_SANITIZE_NUMBER_INT);
 
 $titulo_pagina = "Faça seu Feedback";
 require 'header.php';
+
+
+if(isAdmin()){
+?>
+<div class="alert alert-danger m-4">
+    <h4>Você não pode realizar está operação!</h4>
+    <p>
+        Está ação resultaria em um erro, para enviar um feedback, você deve iniciar a sessão como um usuário comum. 
+    </p>
+</div>
+<?php 
+}
 ?>
 <div class="row justify-content-center align-items-center">
     <div class="col-6">
@@ -22,9 +33,9 @@ require 'header.php';
             <div class="form-floating">
                 <input type="hidden" name="id_usuario" value="<?=id_usuario();?>"/>
                 <input type="hidden" name="id_morador" value="<?=$id_morador;?>"/>
-                    <textarea class="form-control" name="feedback" id="feedback"  placeholder="" required></textarea>
+                    <textarea class="form-control" name="feedback" id="feedback"  <?php if(isAdmin()){ echo "disabled"; }else { echo " "; }?> required></textarea>
                 <label for="feedback">Feedback</label>
-                    <button type="submit" class="btn btn-success my-3">Enviar</button>
+                    <button type="submit" class="btn btn-success my-3" <?php if(isAdmin()){ echo "disabled"; }else { echo " "; }?> >Enviar</button>
             </div>
         </form>
     </div>
